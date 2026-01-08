90c356a1c7bde0087f52a91c93e557efecb6073c
Sillou <ldsbantoine@gmail.com>
2026-01-08 22:24:16 +0100

    user out ouf order

---
+++ src/pipex.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alubrano <alubrano@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/08 21:33:53 by alubrano          #+#    #+#             */
+/*   Updated: 2026/01/08 21:33:53 by alubrano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	if (argc == 5)
+	{
+
+	}
+	else 
+	{
+		ft_printf("Please enter right ags\n");
+		ft_printf("EX : ./pipex <file1> <cmd1> <cmd2> <file2>n");
+	}
+
+}

6711f03eb6e8fa6b79d86bebd3496bac76f1dfe0
Sillou <ldsbantoine@gmail.com>
2026-01-12 23:10:12 +0100

    essaie nul

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: alubrano <alubrano@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 21:33:53 by alubrano          #+#    #+#             */
-/*   Updated: 2026/01/12 09:35:35 by alubrano         ###   ########.fr       */
+/*   Updated: 2026/01/09 17:38:17 by alubrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,53 @@
 
 pid_t	ft_cmd(t_pipex *pipex, char *argv)
 {
-	int	fds[2];
+	pid_t	pid;
+	int		fds[2];
 
 	if (pipe(fds) == -1)
 		perror("Pipe");
-	pipex->pids = fork();
-	if (pipex->pids == -1)
+	pid = fork();
+	if (pid == -1)
 		perror("Fork");
-	if (pipex->pids == 0)
+	if (pid == 0)
 	{
-		close(fds[0]);
-
+		dup2(pipex->prev_in, 0);
 		dup2(fds[1], 1);
+		close(fds[1]);
+		close(fds[0]);
+		close(pipex->prev_in);
+		ft_exec(pipex, argv);
 	}
-
-
+	else
+	{
+		close(fds[1]);
+		close(pipex->prev_in);
+		pipex->prev_in = fds[0];
+	}
+	return (pid);
 }
 
 pid_t	ft_cmd_last(t_pipex *pipex, char *argv)
 {
+	pid_t	pid;
 
+	pid = fork();
+	if (pid == -1)
+		perror("Fork");
+	if (pid == 0)
+	{
+		dup2(pipex->prev_in, 0);
+		dup2( pipex->file_out, 1);
+		close(pipex->prev_in);
+		close(pipex->prev_out);
+		ft_exec(pipex, argv);
+	}
+	else
+	{
+		close(pipex->prev_in);
+		close(pipex->file_out);
+	}
+	return (pid);
 }
 
 int	ft_open(char *argv, int z, t_pipex	*pipex)
@@ -41,7 +68,7 @@ int	ft_open(char *argv, int z, t_pipex	*pipex)
 	int	rest;
 
 	rest = -1;
-	if (z == 0)
+	if (z == 0) 
 		rest = open(argv, O_WRONLY|O_CREAT|O_TRUNC, 0644);
 	else if (z == 1)
 		rest = open(argv, O_RDONLY);
@@ -56,16 +83,16 @@ int	ft_open(char *argv, int z, t_pipex	*pipex)
 t_pipex	*ft_ini_pipex(int argc, char **argv, char **envp)
 {
 	t_pipex *pipex;
-
+	
 	pipex = ft_calloc(1, sizeof(t_pipex));
 	if (!pipex)
 		ft_error("Calloc failled", pipex);
 	pipex->pids = malloc(sizeof(pid_t) * pipex->n_cmd);
 	if (!pipex->pids)
-		ft_error("Pipex pids error", pipex);
+		ft_error("Pipex pids error", pipex); 
 	pipex->paths = ft_get_path(envp);
 	if (!pipex->paths)
-		ft_error("Pipex paths erro", pipex);
+		ft_error("Pipex paths erro", pipex); 
 	pipex->n_cmd = argc - 3;
 	pipex->i = 2;
 	pipex->file_in = -1;
@@ -95,7 +122,7 @@ int	main(int argc, char **argv, char **envp)
 		ft_clean_pipex(pipex);
 		return (0);
 	}
-	else
+	else 
 		ft_printf("ERRO EX : ./pipex file1 cmd1 cmd2 file2");
 	return (1);
 }
